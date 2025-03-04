c51f7475dd4b75c173b7e26b39ddee4f56d4cd1c
Bruno Benavent Colomo <brunobenaventcolomo@iMac-de-Bruno.local>
2025-03-04 15:45:26 +0100

    Job to home 4/03/2025

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import type { MenuItem, OrderItem } from '../types'
+
+export default function useOrder() {
+
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+    const addItem =(item: MenuItem) =>{
+        const itemInOrderExist = order.findIndex(itemState => itemState.id === item.id)
+        if(itemInOrderExist >= 0){
+            const updatedOrder = order.map(itemState=> {
+                if(itemState.id ===item.id){
+
+                    return{
+                        ...itemState,
+                        quantity: itemState.quantity + 1
+                    }
+                }
+                return itemState
+
+            })
+            setOrder(updatedOrder)
+
+
+
+           /*   const updatedOrder = [...order]
+
+           updatedOrder[itemInOrderExist].quantity++
+            setOrder(updatedOrder) */
+
+        }else{
+            const newItem: OrderItem = { ...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
+    }
+
+    const removeFromOrder = (platillo: OrderItem)=>{
+        const updatedOrder = order.filter(item => item.id!== platillo.id)
+        setOrder(updatedOrder)
+    }
+  return {
+    addItem,
+    order,
+    removeFromOrder
+
+  }
+
+}
